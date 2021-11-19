27ecc26426dabd5f5c7503dd6532288c100b2472
samuel barasa <sammie@Sammies-MacBook-Pro.local>
2021-11-19 12:51:59 +0300

    Primary Button

---
+++ Bonfire/Views/Components/PrimaryButton.swift
@@ -0,0 +1,34 @@
+//  PrimaryButton.swift
+//  Bonfire
+
+import SwiftUI
+
+struct PrimaryButton: View {
+    
+    var imageName: String
+    var label: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    var body: some View {
+        HStack {
+            Image(systemName: "\(imageName)")
+                //.renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 20, height: 20)
+            Text(label)
+        }
+        .frame(width: 280, height: 50)
+        .background(backgroundColor)
+        .foregroundColor(textColor)
+        .font(.system(size: 20, weight: .bold, design: .default))
+        .cornerRadius(10)
+    }
+}
+
+//struct PrimaryButton_Previews: PreviewProvider {
+//    static var previews: some View {
+//        PrimaryButton()
+//    }
+//}
---
+++ Bonfire/Views/Components/SocialButton.swift
@@ -0,0 +1,20 @@
+//
+//  SocialButton.swift
+//  Bonfire
+//
+//  Created by Sam Wafula on 19/11/2021.
+//
+
+import SwiftUI
+
+struct SocialButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SocialButton_Previews: PreviewProvider {
+    static var previews: some View {
+        SocialButton()
+    }
+}
