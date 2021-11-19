d0af4175c12554ce6a13331bd742d37141fef088
samuel barasa <sammie@Sammies-MacBook-Pro.local>
2021-11-19 12:52:19 +0300

    Social Button

--- Bonfire/Views/Components/SocialButton.swift
+++ Bonfire/Views/Components/SocialButton.swift
@@ -1,20 +1,33 @@
-//
 //  SocialButton.swift
 //  Bonfire
-//
-//  Created by Sam Wafula on 19/11/2021.
-//
 
 import SwiftUI
 
 struct SocialButton: View {
+    var imageName: String
+    var label: String
+    var textColor: Color
+    var backgroundColor: Color
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Image(systemName: "\(imageName)")
+                //.renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 20, height: 20)
+            Text(label)
+        }
+        .frame(width: 280, height: 50)
+        .background(backgroundColor)
+        .foregroundColor(textColor)
+        .font(.system(size: 20, weight: .bold, design: .default))
+        .cornerRadius(100)
     }
 }
 
-struct SocialButton_Previews: PreviewProvider {
-    static var previews: some View {
-        SocialButton()
-    }
-}
+//struct SocialButton_Previews: PreviewProvider {
+//    static var previews: some View {
+//        SocialButton()
+//    }
+//}
